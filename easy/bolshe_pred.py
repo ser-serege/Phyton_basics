a9bb9f68180722ef9f407f4a418b841479301cea
ser-serege <37813655+ser-serege@users.noreply.github.com>
2020-04-28 22:27:50 +0300

    Update and rename bolshe_pred.py to easy/bolshe_pred.py

--- easy/bolshe_pred.py
+++ easy/bolshe_pred.py
@@ -1,7 +1,7 @@
 a = input().split()
 b = []
 for i in range(1, len(a)):
-    if int(a[i]) > int(a[i-1]):
+    if int(a[i]) > int(a[i - 1]):
         b += a[i]
         print(' '.join(b))
     elif int(a[i]) < 0:
