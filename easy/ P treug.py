dd03273f218a5edc0ff9767b183bccc8872dda12
ser-serege <37813655+ser-serege@users.noreply.github.com>
2020-04-28 22:29:29 +0300

    Update and rename P treug.py to easy/ P treug.py

--- easy/ P treug.py
+++ easy/ P treug.py
@@ -9,9 +9,9 @@ y3 = int(input())
 
 def Ptreug(x1, y1, x2, y2, x3, y3):
 
-    m = sqrt((x1 - x2)**2 + (y1 - y2)**2)
-    n = sqrt((x1 - x3)**2 + (y1 - y3)**2)
-    b = sqrt((x2 - x3)**2 + (y2 - y3)**2)
+    m = sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+    n = sqrt((x1 - x3) ** 2 + (y1 - y3) ** 2)
+    b = sqrt((x2 - x3) ** 2 + (y2 - y3) ** 2)
     p = m + n + b
     return p
 
